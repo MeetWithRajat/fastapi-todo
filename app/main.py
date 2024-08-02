8365cd1f1ee12e4799d7e8428d270a8c7d681e3f
MeetWithRajat <meetwithrajat@gmail.com>
2024-08-02 22:11:37 +0530

    put

--- app/main.py
+++ app/main.py
@@ -51,3 +51,17 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     db.add(todo_model)
     db.commit()
 
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(ge=1)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model:
+        todo_model.title = todo_request.title
+        todo_model.description = todo_request.description
+        todo_model.priority = todo_request.priority
+        todo_model.complete = todo_request.complete
+        # db.add(todo_model)
+        db.commit()
+    else:
+        raise HTTPException(status_code=404, detail="Todo not found")
+
