15fe6ac5f3b3d2780f000f296d191e0e579c9bd0
MeetWithRajat <meetwithrajat@gmail.com>
2024-08-03 00:18:38 +0530

    folder restructure and db changed

--- models.py
+++ models.py
@@ -1,5 +1,17 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
 class Todos(Base):
@@ -10,4 +22,5 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
     
