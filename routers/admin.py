a8486139c15c818e377e14bc26bda06f508d5f3d
MeetWithRajat <meetwithrajat@gmail.com>
2024-08-04 23:08:29 +0530

    refactor

--- routers/admin.py
+++ routers/admin.py
@@ -1,8 +1,8 @@
 from models import Todos
 from starlette import status
 from typing import Annotated
-from sqlalchemy.orm import Session
 from database import SessionLocal
+from sqlalchemy.orm import Session
 from .auth import get_current_user
 from fastapi import Path, APIRouter, Depends, HTTPException
 
--- routers/todos.py
+++ routers/todos.py
@@ -1,10 +1,10 @@
 from models import Todos
 from starlette import status
 from typing import Annotated
-from sqlalchemy.orm import Session
-from pydantic import BaseModel, Field
 from database import SessionLocal
+from sqlalchemy.orm import Session
 from .auth import get_current_user
+from pydantic import BaseModel, Field
 from fastapi import Path, APIRouter, Depends, HTTPException
 
 
