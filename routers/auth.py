64f9327576ccfcf07ae5702d97454f4db5ed7db8
MeetWithRajat <meetwithrajat@gmail.com>
2024-08-04 23:19:27 +0530

    time increased

--- routers/auth.py
+++ routers/auth.py
@@ -98,7 +98,7 @@ async def create_user(db: db_dependency, user_request: UserRequest):
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if user:
-        token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
+        token = create_access_token(user.username, user.id, user.role, timedelta(minutes=30))
         return {"access_token": token, "token_type": "bearer"}
     else:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
