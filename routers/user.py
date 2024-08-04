ed6f5c9f3d669de08b6b53fbba5463010727c72e
MeetWithRajat <meetwithrajat@gmail.com>
2024-08-04 23:04:32 +0530

    refactor

--- routers/user.py
+++ routers/user.py
@@ -1,10 +1,9 @@
-from pydantic import BaseModel, Field
-
 from models import Users
 from starlette import status
 from typing import Annotated
-from sqlalchemy.orm import Session
 from database import SessionLocal
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 from .auth import get_current_user, bcrypt_context
 from fastapi import APIRouter, Depends, HTTPException
 
