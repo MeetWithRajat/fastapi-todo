7d7da30042b21f763ef940fad345b5d9210b1d28
MeetWithRajat <meetwithrajat@gmail.com>
2024-08-10 23:09:02 +0530

    test main

---
+++ test/test_main.py
@@ -0,0 +1,13 @@
+from main import app
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+
+client = TestClient(app)
+
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"status": "Healthy"}
